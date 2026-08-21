6fae8dab177bda6d0a9bfdea1410d531dde0bc66
Nataraj-EL <natarajel.dev@gmail.com>
2026-08-21 06:35:51 +0530

    config: run next dev and build with --webpack and customize devtoolModuleFilenameTemplate for relative source map paths

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['127.0.0.1:3000', 'localhost:3000', '127.0.0.1', 'localhost'],
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.output.devtoolModuleFilenameTemplate = 'webpack:///[resource-path]';
+      config.output.devtoolFallbackModuleFilenameTemplate = 'webpack:///[resource-path]?[hash]';
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
